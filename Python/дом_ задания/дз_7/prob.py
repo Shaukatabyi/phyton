2ca94dff8c52179e740ad617870b078bd6f87a35
Шаукат <shaukat.off@mail.ru>
2023-02-09 22:13:42 +0300

    дз7 тугодум

--- Python/дом_ задания/дз_7/prob.py
+++
@@ -1,8 +0,0 @@
-  
-def pot(operation, num_rows = 6, num_columns = 6):
-    for i in range(1, num_rows+1):
-        for j in range(1, num_columns+1):
-            print(operation(i, j), end = ' ')
-        print()
-
-pot(lambda x, y: x*y)
