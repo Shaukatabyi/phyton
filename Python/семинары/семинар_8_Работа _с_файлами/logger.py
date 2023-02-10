b9c22f89508f89bf237aee10a7b9030da1be0c67
Шаукат <shaukat.off@mail.ru>
2023-02-11 01:51:30 +0300

    завис

---
+++ Python/семинары/семинар_8_Работа _с_файлами/logger.py
@@ -0,0 +1,15 @@
+def input_data():
+    pass
+
+def print_data():
+    print('1 фаил: ')
+    with open('data_first_variant.csv', 'r', encoding = 'utf-8') as file:
+        data_first = file.readlines()
+        print(data_first)
+
+
+def put_data():
+    pass
+
+def delete_data():
+    pass
